650f6f2deb607990aa2652a892e57691f896ab13
Andy Cormack <incurse@gmail.com>
2024-08-24 01:20:35 +0100

    feat: add isCompleted task mutation

--- src/convex/tasks.ts
+++ src/convex/tasks.ts
@@ -1,9 +1,22 @@
-import { query } from './_generated/server';
+import { v } from 'convex/values'
+import { mutation, query } from './_generated/server'
 
 export const get = query({
-  args:{},
+  args: {},
   handler: async (ctx) => {
-    const tasks = await ctx.db.query('tasks').collect();
-    return tasks.map((task) => ({ ...task, assigner: 'tom' }));
+    const tasks = await ctx.db.query('tasks').collect()
+    return tasks.map((task) => ({ ...task, assigner: 'Andy' }))
   },
-});
+})
+
+export const updateCompleted = mutation({
+  args: {
+    id: v.id('tasks'),
+    isCompleted: v.boolean(),
+  },
+  handler: async (ctx, { id, isCompleted }) => {
+    const task = await ctx.db.get(id)
+    await ctx.db.patch(id, { isCompleted: isCompleted ?? !task.isCompleted })
+    return task
+  },
+})
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,15 @@
 <script lang="ts">
-  import { useQuery } from 'convex-svelte'
-  import { api } from '../convex/_generated/api.js'
   import { Checkbox } from '$lib/components/ui/checkbox/index.js'
+  import type { Id } from '@/convex/_generated/dataModel.js'
+  import { useConvexClient, useQuery } from 'convex-svelte'
+  import { api } from '../convex/_generated/api.js'
 
+  const client = useConvexClient()
   const query = useQuery(api.tasks.get, {})
+
+  function onCompletedChanged(id: Id<'tasks'>, isCompleted: boolean) {
+    client.mutation(api.tasks.updateCompleted, { id, isCompleted })
+  }
 </script>
 
 {#if query.isLoading}
@@ -14,9 +20,14 @@
   <ul>
     {#each query.data as task}
       <li>
-        <Checkbox bind:checked={task.isCompleted} />
+        <Checkbox
+          checked={task.isCompleted}
+          onCheckedChange={(isChecked) =>
+            onCompletedChanged(task._id, isChecked)}
+        />
+        {task.isCompleted ? '✅' : '❌'}
         <span>{task.text}</span>
-        <span>assigned by {task.assigner}</span>
+        <span class="text-muted-foreground">assigned by {task.assigner}</span>
       </li>
     {/each}
   </ul>

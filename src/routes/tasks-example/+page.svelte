121fa10bfa092468b1d22250e40670fae9f5470d
Andy Cormack <incurse@gmail.com>
2024-08-25 21:09:57 +0100

    feat: loading state for checkbox mutates + move tasks page

--- src/routes/tasks-example/+page.svelte
+++ src/routes/tasks-example/+page.svelte
@@ -1,14 +1,18 @@
 <script lang="ts">
   import { Checkbox } from '$lib/components/ui/checkbox/index.js'
+  import { api } from '@/convex/_generated/api.js'
   import type { Id } from '@/convex/_generated/dataModel.js'
   import { useConvexClient, useQuery } from 'convex-svelte'
-  import { api } from '../convex/_generated/api.js'
+  import { SvelteMap } from 'svelte/reactivity'
 
   const client = useConvexClient()
   const query = useQuery(api.tasks.get, {})
 
-  function onCompletedChanged(id: Id<'tasks'>, isCompleted: boolean) {
-    client.mutation(api.tasks.updateCompleted, { id, isCompleted })
+  const loadingMap = new SvelteMap<Id<'tasks'>, boolean>()
+  async function onCompletedChanged(id: Id<'tasks'>, isCompleted: boolean) {
+    loadingMap.set(id, true)
+    await client.mutation(api.tasks.updateCompleted, { id, isCompleted })
+    loadingMap.delete(id)
   }
 </script>
 
@@ -22,10 +26,11 @@
       <li>
         <Checkbox
           checked={task.isCompleted}
-          onCheckedChange={(isChecked) =>
-            onCompletedChanged(task._id, isChecked as boolean)}
+          onCheckedChange={async (isChecked) => {
+            onCompletedChanged(task._id, isChecked as boolean)
+          }}
+          disabled={!!loadingMap.get(task._id)}
         />
-        {task.isCompleted ? '✅' : '❌'}
         <span>{task.text}</span>
         <span class="text-muted-foreground">assigned by {task.assigner}</span>
       </li>

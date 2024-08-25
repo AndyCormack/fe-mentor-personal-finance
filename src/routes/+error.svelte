37e611043aa01f9e7f78428a22226b3ab4e15bb1
Andy Cormack <incurse@gmail.com>
2024-08-25 21:08:00 +0100

    feat: add root error page layout

---
+++ src/routes/+error.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  import { page } from '$app/stores'
+</script>
+
+<div
+  class="prose dark:prose-invert flex h-full flex-col justify-center text-center"
+>
+  <h1 class="m-0">Error {$page.status}</h1>
+  <h3 class="m-0 opacity-50">{$page.error?.message}</h3>
+</div>

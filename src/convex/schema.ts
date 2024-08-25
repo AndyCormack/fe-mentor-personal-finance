0338625d38f935614ca8056bfa4e2878020acb4f
Andy Cormack <incurse@gmail.com>
2024-08-26 00:31:01 +0100

    feat: example data and schema for users and transactions

--- src/convex/schema.ts
+++ src/convex/schema.ts
@@ -6,4 +6,20 @@ export default defineSchema({
     isCompleted: v.boolean(),
     text: v.string(),
   }),
+
+  users: defineTable({
+    avatar: v.string(),
+    email: v.string(),
+    name: v.string(),
+  }),
+
+  transactions: defineTable({
+    amount: v.float64(),
+    avatar: v.string(),
+    category: v.string(),
+    date: v.string(),
+    name: v.string(),
+    recurring: v.boolean(),
+    user: v.id('users'),
+  }),
 })
